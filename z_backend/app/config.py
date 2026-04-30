51f10138356f5286c2a0fc6324e3c810b5e92ada
nKrasnikov <kolyakrasnikov36987@gmail.com>
2026-04-30 18:00:53 +0300

    api key remove

--- z_backend/app/config.py
+++ z_backend/app/config.py
@@ -9,7 +9,7 @@ class Settings:
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    YANDEX_GPT_API_KEY: str = os.getenv("YANDEX_GPT_API_KEY", "AQVNz6jsbYENI6JH2AN76oxglLieRdPW-w20ZYep")
+    YANDEX_GPT_API_KEY: str = os.getenv("YANDEX_GPT_API_KEY", "")
     YANDEX_FOLDER_ID: str = os.getenv("YANDEX_FOLDER_ID", "b1giaber8geti5ev12ka")
     YANDEX_GPT_URL: str = os.getenv("YANDEX_GPT_URL", "https://llm.api.cloud.yandex.net/v1/chat/completions")
     REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
